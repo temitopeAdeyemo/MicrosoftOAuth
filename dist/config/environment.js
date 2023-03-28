a0ac8bc0e62d4358debe9d5fcfcb38d7471fcbc9
temiAdeyemo <temitopejulius9@gmail.com>
2023-03-28 02:11:06 +0100

    Chore: Check

--- dist/config/environment.js
+++ dist/config/environment.js
@@ -10,5 +10,5 @@ exports.default = {
     baseUrl: process.env.BASE_URL || `https://localhost:${process.env.PORT || '9000'}`,
     secret: process.env.SECRET || 'secret',
     clientID: process.env.CLIENT_ID || '1b0b1e2b-308d-4c21-8de0-617453cd4665',
-    clientSecret: process.env.CLIENT_SECRET || "CSG8Q~-Aom8CBHiU2Vo1rv2AsuYzoSFdN0Rhda2i"
+    clientSecret: process.env.CLIENT_SECRET || 'CSG8Q~-Aom8CBHiU2Vo1rv2AsuYzoSFdN0Rhda2i',
 };
--- src/config/environment.ts
+++ src/config/environment.ts
@@ -7,5 +7,6 @@ export default {
     process.env.BASE_URL || `https://localhost:${process.env.PORT || '9000'}`,
   secret: process.env.SECRET || 'secret',
   clientID: process.env.CLIENT_ID || '1b0b1e2b-308d-4c21-8de0-617453cd4665',
-  clientSecret: process.env.CLIENT_SECRET || "CSG8Q~-Aom8CBHiU2Vo1rv2AsuYzoSFdN0Rhda2i"
+  clientSecret:
+    process.env.CLIENT_SECRET || 'CSG8Q~-Aom8CBHiU2Vo1rv2AsuYzoSFdN0Rhda2i',
 };
